a892dcb01aed9189a8110f4feffdb79b9db8117b
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-07-26 14:01:12 -0500

    Initial commit

---
+++ src/domain/App/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./App.module.scss";
+import Layout from "domain/Layout";
+
+function App() {
+  return <Layout></Layout>;
+}
+
+export default App;
---
+++ src/domain/Layout/Layout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Layout.module.scss";
+
+function Layout({ children }) {
+  return <div>{children}</div>;
+}
+
+export default Layout;
