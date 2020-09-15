16bde3ec18e4a2684825f4b56b01e2ddb70df5f4
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-09-14 19:58:16 -0500

    Created NotFound view

---
+++ src/views/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NotFound.module.scss";
+
+function NotFound(props) {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  );
+}
+
+export default NotFound;
